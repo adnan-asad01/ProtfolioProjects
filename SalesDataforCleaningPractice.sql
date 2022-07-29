56dabb3a0a3eaebc21f2751b309cece4b95bd8b6
adnan-asad01 <adnan.asad01@gmail.com>
2022-07-29 23:36:03 +0200

    SQLJournal set up

--- SalesDataforCleaningPractice.sql
+++ SalesDataforCleaningPractice.sql
@@ -1,30 +1,30 @@
 /*
 Cleaning Data in SQL Queries
 */
---CREATE TABLE EmployeeDemo
---(EmployeeID int,
---FirstName varchar(50),
---LastName varchar(50),
---Age int,
---Gender varchar(50)
---)
+CREATE TABLE EmployeeDemo
+(EmployeeID int,
+FirstName varchar(50),
+LastName varchar(50),
+Age int,
+Gender varchar(50)
+)
 
---CREATE TABLE EmplyeeSalary
---(EmployeeID int,
---JobTitel varchar(50),
---Salary int,
---)
+CREATE TABLE EmplyeeSalary
+(EmployeeID int,
+JobTitel varchar(50),
+Salary int,
+)
 
---Insert into EmployeeDemo VALUES
---(1001, 'Jim', 'Halpert', 30, 'Male'),
---(1002, 'Pam', 'Beasley', 30, 'Female'),
---(1003, 'Dwight', 'Schrute', 29, 'Male'),
---(1004, 'Angela', 'Martin', 31, 'Female'),
---(1005, 'Toby', 'Flenderson', 32, 'Male'),
---(1006, 'Michael', 'Scott', 35, 'Male'),
---(1007, 'Meredith', 'Palmer', 32, 'Female'),
---(1008, 'Stanley', 'Hudson', 38, 'Male'),
---(1009, 'Kevin', 'Malone', 31, 'Male')
+Insert into EmployeeDemo VALUES
+(1001, 'Jim', 'Halpert', 30, 'Male'),
+(1002, 'Pam', 'Beasley', 30, 'Female'),
+(1003, 'Dwight', 'Schrute', 29, 'Male'),
+(1004, 'Angela', 'Martin', 31, 'Female'),
+(1005, 'Toby', 'Flenderson', 32, 'Male'),
+(1006, 'Michael', 'Scott', 35, 'Male'),
+(1007, 'Meredith', 'Palmer', 32, 'Female'),
+(1008, 'Stanley', 'Hudson', 38, 'Male'),
+(1009, 'Kevin', 'Malone', 31, 'Male')
 
 Insert Into EmployeeSalary VALUES
 (1001, 'Salesman', 45000),
